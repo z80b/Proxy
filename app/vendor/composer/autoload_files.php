fd26305c282fef80c2c1d0b1b94fa3b829dc98ff
Stitch <akozlov78@gmail.com>
2016-05-05 11:03:54 +0300

    Fix composer autoloader

--- app/vendor/composer/autoload_files.php
+++ app/vendor/composer/autoload_files.php
@@ -8,5 +8,4 @@ $baseDir = dirname($vendorDir);
 return array(
     'fc73bab8d04e21bcdda37ca319c63800' => $vendorDir . '/mikecao/flight/flight/autoload.php',
     '5b7d984aab5ae919d3362ad9588977eb' => $vendorDir . '/mikecao/flight/flight/Flight.php',
-    'c7359326b6707d98bdc176bf9ddeaebf' => $vendorDir . '/catfan/medoo/medoo.php',
 );
